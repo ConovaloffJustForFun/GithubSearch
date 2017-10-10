a52930af70cf7b9c4a19519a6a68c85d03d5c9c6
ConConovaloff <conovaloffforspam@gmail.com>
2017-10-10 13:37:07 +0300

    add "Not found repositories" state for result

--- www/template/result.php
+++ www/template/result.php
@@ -1,12 +1,19 @@
 <div class="result container center">
 <?php
-
 /** @var $result array */
-foreach ($result as $repo): ?>
+if (empty($result)):?>
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2">
-            <a href="<?= $repo['html_url'] ?>"><?= $repo['html_url'] ?></a>
+            Not found repositories with this query.
         </div>
     </div>
-<?php endforeach; ?>
+<?php else: ?>
+    <?php foreach ($result as $repo): ?>
+        <div class="row">
+            <div class="col-sm-8 col-sm-offset-2">
+                <a href="<?= $repo['html_url'] ?>"><?= $repo['html_url'] ?></a>
+            </div>
+        </div>
+    <?php endforeach; ?>
+<?php endif; ?>
 </div>
